f3b5d71d95d6053f2a8741237154065a3c587fb4
Kangaroozac1 <znmiller96@gmail.com>
2021-03-20 13:13:01 -0500

    Update clean up code, switch from swing to react for frontend

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,20 +1,12 @@
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import Backend.JsonFactory;
 import Backend.Randomizer;
-import UI.MainPage;
 
 public class main {
 
     private Map<String, Randomizer> savedRandomizers;
 
     public static void main(String[] args) {
-
-        MainPage mainPage = new MainPage();
 
     }
 
